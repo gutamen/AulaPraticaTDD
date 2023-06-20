7e76b38e1e6d4ac9b7eab5b4d8b78d7c3bcf6209
gutamen <gutamen@live.com>
2023-06-20 15:14:41 -0300

    morte

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,32 @@
-    class Dollar {
-       int amount = 10;
-       Dollar(int amount) {}			
-       void times(int multiplier) {}
-    }	
 
-    public void testMultiplication() {
-       Dollar five = new Dollar(5);
-       five.times(2);
-       assertEquals(10, five.amount);
+
+class Dollar extends Money { 
+    private int amount;
+
+    Dollar(int amount){
+        this.amount= amount;
     }
+    //void times(int multiplier) {
+      //  amount= amount * multiplier;
+    //}
+    Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
+    }
+  
+	
+    public void testMultiplication() {
+      Dollar five = new Dollar(5);
+      assertEquals(new Dollar(10), five.times(2));
+      assertEquals(new Dollar(15), five.times(3));
+    }
+
+    public void testEquality() {
+      assertTrue(new Dollar(5).equals(new Dollar(5)));
+      assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    public boolean equals(Object object)  {
+      Dollar dollar = (Dollar) object;
+      return amount == dollar.amount;
+    }
+}
